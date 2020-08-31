760c0a3c21b20d90c2a58b0fc72e7806ef2dabaf
Gabriel <gabrielfilleti@gmail.com>
2020-08-31 19:07:38 -0300

    add new function check_disk_full and renamed vabiables min_absolute to min_gb and use parameter names

--- git_intro/scripts/all_checks.py
+++ git_intro/scripts/all_checks.py
@@ -6,16 +6,27 @@ def check_reboot():
     """ Return True if the computer has a pending reboot."""
     return os.path.exists("/run/reboot-required")
 
+def check_disk_full(disk, min_gb, min_percent):
+    """Return True if thre isn't enough disk space, False otherwise"""
+    du = shutil.disk_usage(disk)
+    #calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    #calculate how many free gigabytes
+    gigabytes_free = du.free / 2 **30
+    if percent_free < min_percent or gigabytes_free < min_gb:
+        return True
+    return False
+
 
 def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
-    
+    if check_disk_full(disk="/", min_gb=2, min_percent=10):
+        print("Disk full.")
+        sys.exit(1)
+
     print("Everything ok.")
     sys.exit(0)
-
-
-
 
 main()
